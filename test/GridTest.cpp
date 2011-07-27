d413ae99a05e9cbb749b78b806cb1756aa5dd175
Florian Spechtenhauser <f.spechtenhauser@gmail.com>
2011-07-27 16:08:00 +0200

    added new test to gridtest; heavily changed the test-project

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -1,7 +1,43 @@
 #include "GridTest.h"
 
 
-void GridTest::test1()
+void GridTest::setup()
 {
-	TEST_ASSERT(1+1==2);
+	grid1.AddDoor(2,2);
+	grid1.AddDoor(1,8);
+	grid1.AddDoor(7,5);
+
+	grid2.AddDoor(5,5);
+	grid2.AddDoor(9,9);
+	grid2.AddDoor(1,2);
+	grid2.AddDoor(8,6);
+}
+
+void GridTest::tear_down()
+{
+	grid1.ClearGrid();
+	grid2.ClearGrid();
+}
+
+void GridTest::TestDoorInsertion()
+{
+	TEST_ASSERT(grid1.AddDoor(4,4));
+	TEST_ASSERT(grid1.AddDoor(2,2)==false);
+	TEST_ASSERT(grid1.AddDoor(7,6)==false);
+	TEST_ASSERT(grid1.AddDoor(3,3)==false);
+	TEST_ASSERT(grid1.AddDoor(0,9)==false);
+	TEST_ASSERT(grid1.AddDoor(5,10)==false);
+	TEST_ASSERT(grid1.AddDoor(100, -100)==false);
+}
+
+void GridTest::TestRotation1()
+{
+}
+
+void GridTest::TestRotation2()
+{
+}
+
+void GridTest::TestRotation3()
+{
 }
--- test/GridTest.h
+++ test/GridTest.h
@@ -1,14 +1,29 @@
 #include "cpptest.h"
 using namespace std;
 
+#include "Grid.h"
+
 class GridTest : public Test::Suite {
 
 public:
 	GridTest()
 	{
-		TEST_ADD(GridTest::test1);
+		TEST_ADD(GridTest::TestDoorInsertion);
+		TEST_ADD(GridTest::TestRotation1);
+		TEST_ADD(GridTest::TestRotation2);
+		TEST_ADD(GridTest::TestRotation3);
 	}
 
+protected:
+	virtual void setup();
+	virtual void tear_down();
+
 private:
-	void test1();
+	Grid grid1;
+	Grid grid2;
+
+	void TestDoorInsertion();
+	void TestRotation1();
+	void TestRotation2();
+	void TestRotation3();
 };
