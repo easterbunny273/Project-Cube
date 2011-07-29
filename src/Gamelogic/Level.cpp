329d1a64deb57b4785c75beaacce45a188e1de4e
Florian Spechtenhauser <f.spechtenhauser@gmail.com>
2011-07-29 14:43:41 +0200

    added an example for a level-xml; expanded level.h and created empty level.cpp #6

---
+++ src/Gamelogic/Level.cpp
@@ -0,0 +1,19 @@
+#include "Gamelogic/Level.h"
+
+Level::Level(std::string sFilename)
+{
+}
+
+Level::~Level()
+{
+}
+
+bool Level::ReadFromXMLString(std::string sFilename)
+{
+	return false;
+}
+
+bool Level::WriteToXMLString(std::string &sFilename)
+{
+	return false;
+}
