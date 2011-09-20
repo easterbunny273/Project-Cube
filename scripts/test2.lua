db1c220bb22125c0c33b14e4f841417cfff8c0f0
Christian Möllinger <ch.moellinger@gmail.com>
2011-09-20 12:16:13 +0200

    first test with lua

---
+++ scripts/test2.lua
@@ -0,0 +1,5 @@
+function start2()
+  print "lua-method \"start2\": we will try to call a c++ method"
+  TestMethodCalledFromLua(99);
+  TestMethodCalledFromLua(100);
+end
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,69 @@
 #include "TestMain.h"
 #include "Gamelogic/Level.h"
 
+extern "C"
+{
+    #include "lua.h"
+    #include "lualib.h"
+}
+
+#include <luabind/luabind.hpp>
+
+#include <iostream>
+
+void TestMethodCalledFromLua(int a)
+{
+    std::cout << "c++ method called with a=" << a << std::endl;
+}
+
+extern "C"
+{
+    void LuaTest()
+    {
+	// Create a new lua state
+	lua_State *myLuaState = luaL_newstate();
+
+	// load lua libs (io, math, ...)
+	luaL_openlibs(myLuaState);
+
+    	// Connect LuaBind to this lua state
+	luabind::open(myLuaState);
+
+	int iError = luaL_dofile(myLuaState, "scripts/test1.lua");
+	assert (iError == 0);
+
+	int iError2 = luaL_dofile(myLuaState, "scripts/test2.lua");
+	assert (iError2 == 0);
+
+	// Define a lua function that we can call
+	luaL_dostring(
+	myLuaState,
+	"function add(first, second)\n"
+	"  return first + second\n"
+	"end\n"
+	);
+
+	luabind::module(myLuaState) [
+	    luabind::def("TestMethodCalledFromLua", TestMethodCalledFromLua)
+	];
+
+	std::cout << "Result: "
+	<< luabind::call_function<int>(myLuaState, "add", 2, 3)
+	<< std::endl;
+
+	luabind::call_function<void>(myLuaState, "start");
+	luabind::call_function<void>(myLuaState, "start2");
+
+	lua_close(myLuaState);
+
+    }
+}
+
 int main()
 {
-    TestMain::GetInstance()->Run();
-    MainApp::GetInstance()->Run();
+    int a=4;
+
+    LuaTest();
+   // TestMain::GetInstance()->Run();
+   // MainApp::GetInstance()->Run();
 }
